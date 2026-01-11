902173e14ab22cda6b83ed54e0781e93f9ed0937
Ngayka <ngayka@gmail.com>
2026-01-11 16:34:32 +0200

    Add validation. change main.py, tests.confest

---
+++ src/validation/validation/__init__.py
@@ -0,0 +1,10 @@
+from validation.profile import (
+    validate_name,
+    validate_image,
+    validate_gender,
+    validate_birth_date
+)
+
+from validation.orders import is_movie_available
+
+from validation.payments import validate_payment_method
